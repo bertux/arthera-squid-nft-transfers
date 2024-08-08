fe8d961fff20b842bb85a8c5bdf85ce6a0df2dbe
Bertrand Juglas <bertrand.juglas@arthera.net>
2024-08-08 18:19:09 +0200

    Update processor.ts for Arthera

--- src/processor.ts
+++ src/processor.ts
@@ -9,7 +9,17 @@ import {
 import * as erc721 from "./abi/erc721";
 
 export const processor = new EvmBatchProcessor()
-  .setGateway("https://v2.archive.subsquid.io/network/ethereum-mainnet")
+  .setGateway('https://v2.archive.subsquid.io/network/arthera-mainnet')
+  // Chain RPC endpoint is required for
+  //  - indexing unfinalized blocks https://docs.subsquid.io/basics/unfinalized-blocks/
+  //  - querying the contract state https://docs.subsquid.io/evm-indexing/query-state/
+  .setRpcEndpoint({
+    // Set the URL via .env for local runs or via secrets when deploying to Subsquid Cloud
+    // https://docs.subsquid.io/deploy-squid/env-variables/
+    url: assertNotNull(process.env.RPC_ETH_HTTP, 'No RPC endpoint supplied'),
+    // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
+    rateLimit: 10
+  })
   .addTrace({
     type: ["create"],
     transaction: true,
